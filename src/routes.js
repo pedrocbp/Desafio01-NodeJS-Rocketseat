da6ab7e7f413414dc034efbf7a8a1d3665611a57
Pedro <pedromiguelboce@gmail.com>
2025-04-10 22:24:50 -0300

    feat: corrige nome da propriedade 'descriptor' para 'description' e adiciona timestamps nas tarefas

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,7 @@ export const routes = [
     
           const tasks = database.select('tasks', search ? {
             title: search,
-            descriptor: search
+            description: search
           } : null)
     
           return res.end(JSON.stringify(tasks))
@@ -22,12 +22,16 @@ export const routes = [
         method: 'POST',
         path: buildRoutePath('/tasks'),
         handler: (req, res) => {
-          const { title, descriptor } = req.body
-    
+          const { title, description } = req.body
+          const now = new Date();
+
           const task = {
             id: randomUUID(),
             title,
-            descriptor,
+            description,
+            created_at: now,
+            updated_at: null,
+            completed_at: null,
           }
     
           database.insert('tasks', task)
