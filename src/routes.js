0fb421753b05cb13b518908b24bf20a11adba399
Pedro <pedromiguelboce@gmail.com>
2025-04-12 14:53:45 -0300

    feat: implementa funiconalidade de deletar uma task

--- src/routes.js
+++ src/routes.js
@@ -1,43 +1,59 @@
-import { randomUUID } from 'node:crypto'
-import { Database } from './database.js'
-import { buildRoutePath } from './utils/build-route-path.js'
+import { randomUUID } from "node:crypto";
+import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
-const database = new Database()
+const database = new Database();
 export const routes = [
-    {
-        method: 'GET',
-        path: buildRoutePath('/tasks'),
-        handler: (req, res) => {
-          const { search } = req.query
-    
-          const tasks = database.select('tasks', search ? {
-            title: search,
-            description: search
-          } : null)
-    
-          return res.end(JSON.stringify(tasks))
-        }
-    },
-    {
-        method: 'POST',
-        path: buildRoutePath('/tasks'),
-        handler: (req, res) => {
-          const { title, description } = req.body
-          const now = new Date();
+  {
+    method: "GET",
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      const { search } = req.query;
 
-          const task = {
-            id: randomUUID(),
-            title,
-            description,
-            created_at: now,
-            updated_at: null,
-            completed_at: null,
-          }
-    
-          database.insert('tasks', task)
-    
-          return res.writeHead(201).end()
-        }
-    },
+      const tasks = database.select(
+        "tasks",
+        search
+          ? {
+              title: search,
+              description: search,
+            }
+          : null
+      );
 
-]
+      return res.end(JSON.stringify(tasks));
+    },
+  },
+  {
+    method: "POST",
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      const { title, description } = req.body;
+      const now = new Date();
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        created_at: now,
+        updated_at: null,
+        completed_at: null,
+      };
+
+      database.insert("tasks", task);
+
+      return res.writeHead(201).end();
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+      const now = new Date();
+
+      database.delete("tasks", id);
+
+      return res.writeHead(204).end();
+    },
+  },
+];
